cbfa337651206b299e75a6dfef5ce17b0d644d76
Willian Duarte <willian@dzigual.com.br>
2020-12-23 18:00:31 -0300

    Subindo arquivos...

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Nextjs With P5js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js</a> with <a href="https://p5js.org">P5.js</a>
+        </h1>
+        <iframe width="100%" height="600px" src="https://editor.p5js.org/willianxz/embed/NgdmAg30l"></iframe>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+      
+    </div>
+  )
+}
